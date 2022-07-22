e3f43fd152b58db6f5dececec2eb0ad13e45c8a5
fahminlb33 <fahminlb33@gmail.com>
2022-07-22 15:30:20 +0700

    Add mongodb dependency

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const Redis = require("ioredis");
+const mongodb = require("mongodb");
 const express = require("express");
 
+const mongo = new mongodb.MongoClient(process.env.MONGODB_URL);
+
 const redis = new Redis.default({
-    host: "localhost",
-    port: 6379,
-    password: "testing"
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
 });
 
 const app = express();
-app.get("/counter", async function (req, res) {
+app.get("/redis-counter", async function (req, res) {
     const count = await redis.incr("counter");
     return res.send(`Count: ${count}`);
 });
 
-app.listen(3000, function () {
+app.get("/mongo-counter", async function (req, res) {
+    const count = await mongo.db("test").collection("test").countDocuments();
+    return res.send(`Count: ${count}`);
+});
+
+app.listen(3000, async function () {
     console.log("Listening on port 3000");
+    await mongo.connect();
+
+    mongo.db("test").collection("test").insertOne({
+        timestamp: new Date()
+    });
 }); 
